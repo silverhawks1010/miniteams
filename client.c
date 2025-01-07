8c655b3827e8cc845bf41e7ea9da3163b6891885
silverhawks1010 <thekeke60@gmail.com>
2025-01-07 11:30:21 +0100

    message system

--- client.c
+++ client.c
@@ -2,6 +2,38 @@
 // Created by silverhawks on 06/01/25.
 //
 
-void main() {
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
 
- }
+void char_to_binary(char c, char *binary) {
+    int ascii_value = (int)c;
+    for (int i = 7; i >= 0; i--) {
+        binary[7 - i] = (ascii_value & (1 << i)) ? '1' : '0';
+    }
+    binary[8] = '\0';
+}
+
+int main(int argc, char *argv[]) {
+	int pid = atoi(argv[1]);
+	char *message = argv[2];
+
+    for (int i = 0; i < strlen(message); i++) {
+        char binary[9];
+        char_to_binary(message[i], binary);
+        printf("Sending message to %d\n", pid);
+        for (int j = 0; j < 8; j++) {
+            if (binary[j] == '1') {
+                kill(pid, SIGUSR1);
+            } else {
+                kill(pid, SIGUSR2);
+            }
+            usleep(1000);
+        }
+    }
+    printf("Sending final message to %d\n", pid);
+    kill(pid, SIGQUIT);
+}
+
--- server.c
+++ server.c
@@ -2,6 +2,53 @@
 // Created by silverhawks on 06/01/25.
 //
 
-void main() {
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
+int bits = 0;
+char mots = 0;
+char message[256]; // Buffer to store the message
+int message_index = 0;
+
+void handler(int sig) {
+    if (sig == SIGQUIT) {
+        printf("Final message: %s\n", message);
+        message_index = 0;
+        bits = 0;
+        mots = 0;
+        memset(message, 0, sizeof(message));  // Clear the entire message buffer
+        return;
+    }
+    if (sig == SIGUSR1) {
+        mots |= (1 << (7 - bits)); // Set the bit to 1
+    } else if (sig == SIGUSR2) {
+        mots &= ~(1 << (7 - bits)); // Set the bit to 0
+    }
+    bits++;
+    if (bits == 8) {
+        message[message_index++] = mots; // Add character to message
+        message[message_index] = '\0'; // Null-terminate the string
+        printf("Received character: %c\n", mots);
+        printf("Current message: %s\n", message);
+        bits = 0;
+        mots = 0;
+    }
+    fflush(stdout);
+}
+
+int main() {
+      printf("Server PID: %d\n", getpid());
+      printf("Waiting for signals...\n");
+
+    signal(SIGUSR1, handler);
+    signal(SIGUSR2, handler);
+    signal(SIGQUIT, handler);
+
+
+    while(1) {
+        pause();
+    }
+    return 0;
 }
